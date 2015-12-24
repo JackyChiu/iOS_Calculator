95e8ab5ce4bc840f56a0b6f6e06a19edee5ed093
Jacky Chiu <jacky.chiu888@gmail.com>
2015-12-24 18:58:53 -0500

    first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jacky Chiu on 2015-12-24.
+//  Copyright © 2015 Jacky Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
