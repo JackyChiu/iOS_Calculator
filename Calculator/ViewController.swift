ec60cedc6f173a634f5b420663beec75978b3794
Jacky Chiu <jacky.chiu888@gmail.com>
2015-12-25 01:18:09 -0500

    Have numbers appear on operations line, working on calculation

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var Answer: Int = 0
+    var operations = ["=","+","-","X","/"]
+    
+    func isIn(varible:String,list: [String])->Bool{
+        for i in list{
+            if varible == i{
+                return true
+            }
+        }
+        return false
+    }
+    
+    @IBOutlet weak var operationsLabel: UILabel!
+    
+    @IBAction func clearButton(sender: UIButton) {
+        operationsLabel.text = ""
+    }
+ 
+    @IBAction func oneButton(sender: UIButton) {
+        operationsLabel.text! += "1"
+    }
 
+    @IBAction func twoButton(sender: UIButton) {
+        operationsLabel.text! += "2"
+    }
+
+    @IBAction func plusButton(sender: UIButton) {
+        operationsLabel.text! += "+"
+    }
+    
+    @IBAction func equalButton(sender: UIButton) {
+        
+        var operationsLine:String
+        //var input1:Int
+        //var input2:Int
+        //var operation:String
+        
+        operationsLabel.text! += "="
+        operationsLine = operationsLabel.text!
+        
+        for i in operationsLine.characters{
+            if isIn(String(i), list: operations){
+                print(i)
+            }
+        }
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
